5b7cb5ee951a1e406fee5517c79171336bf7c660
ANGELO DIANCARLO QUEZADA GALLARDO <85042400+An-d0811@users.noreply.github.com>
2024-07-31 09:22:36 -0600

    Add files via upload

---
+++ sitio5/js/script.js
@@ -0,0 +1,24 @@
+console.log("Hola mundo soy JAVA Script desde el archovo JS")
+
+//Declarar varaibles
+var texto = "Hola Mundo";
+var numero1 = 1;
+var booleano = true;
+var flotante = 23.4;
+
+//Operadores
+var suma = numero1+3;
+console.log("La suma de numero1 + 3 es igual a: "+ suma)
+
+var multi = numero1*3;
+console.log("La multiplñicacion de numero1 * 3 es igual a: "+ multi)
+
+//Arreglo
+var arreglo = ["Fresa","Manzana", "Banano", "Uva"];
+console.log(arreglo);
+console.log("La cantidad de frutas",arreglo.length);
+
+//Ciclos
+for (var i=1;i<10;i++){
+    console.log("El ciclo for cuenta el recorrido del mismo ", i)
+}
